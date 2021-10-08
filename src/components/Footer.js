0253bbe6c4e3ca66b82fbb6f29854512d7f84841
Prakash Dhayal <nitianprakash2020@gmail.com>
2021-10-08 20:56:06 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (<>
+        <footer>
+            <h3>Made by Prakash Dhayal</h3>
+            <h4>Software Engineering Project</h4>
+            <p>Contact Me: <a href="https://www.linkedin.com" target="_blank">LinkedIn</a></p>
+        </footer>
+    </>);
+}
+
+export default Footer;
